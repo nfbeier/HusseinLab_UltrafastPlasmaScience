ed0768228d7762f0ba64b1696414647ea93fbf74
Nick Beier <nfbeier@gmail.com>
2024-07-03 16:13:45 -0600

    Included saving class in Thorlabs Widget
    Thorlabs save thread copied over from UCI code. Need to switch configuration for this spectrometer. Ideally change from pandas to h5py

--- Hardware/Thorlabs CCS200/thorlabsCCS200_Widget.py
+++ Hardware/Thorlabs CCS200/thorlabsCCS200_Widget.py
@@ -1,10 +1,11 @@
 import numpy as np
 from pyvisa import ResourceManager
-from instrumental import instrument, list_instruments
+from instrumental import instrument#, list_instruments
+from instrumental.drivers.spectrometers.thorlabs_ccs import list_instruments
 import os, sys, time
 from PyQt5 import QtCore, QtGui, QtWidgets
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-
+import pandas as pd
 cwd = os.getcwd()
 # Check if 'HusseinLab_UltrafastPlasmaScience' is in the components
 if 'HusseinLab_UltrafastPlasmaScience' not in cwd.split(os.path.sep):
@@ -16,6 +17,56 @@ cwd = os.path.sep.join(cwd.split(os.path.sep)[:cwd.split(os.path.sep).index('Hus
 sys.path.insert(0,cwd)
 
 from thorlabsCCS200_GUI import Ui_Form
+
+class SaveThread(QtCore.QThread):
+    def __init__(self, scans, template, exposure, shotNumber):
+        super(SaveThread, self).__init__()
+        self.scans = scans
+        self.exposureTime = exposure
+        self.shotNumber = shotNumber
+        [self.folder, self.fileName, self.date, self.shotTracker] = template
+        self.shotDict = {}
+        if self.folder == None:
+            self.folder = r"D:\Data\Default Save Directory\Flame"
+        if self.fileName == None:
+            self.fileName = "No_Name"
+        if self.date == None:
+            self.date = True
+ 
+    def run(self):
+        if self.date:
+            now = QtCore.QDateTime.currentDateTime()
+            nowString = now.toString(QtCore.Qt.ISODate)
+            formatDate = nowString.replace(":","").replace("-","").replace("T","_")
+            self.fileName = formatDate + "_"+ self.fileName
+            self.shotDict = {"Date": formatDate}
+        
+        if self.shotTracker:
+            self.shotDict["Shot Number"] = self.shotNumber 
+        self.shotDict["Exposure Time"] = self.exposureTime
+        self.shotDict["Experimental Campaign"] = "Testing" 
+        self.shotDict["Device"] = "Thorlabs CCS200"              
+        self.shotDict["Flame Spectra"] = self.scans
+        self.shotDict["Flame Spectra Number"] = len(self.scans)
+        
+        df = pd.Series(self.shotDict)
+        
+        if self.shotTracker:
+            fullName = r"%s/%s_Shot%05d.h5"%(self.folder,self.fileName,self.shotNumber)
+            print(fullName)
+            df.to_hdf(fullName,key='s', mode='w')
+        else:
+            fullName = r"%s/%s.h5"%(self.folder,self.fileName)
+            print(fullName)
+            df.to_hdf(fullName,key='s', mode='w')
+        
+        for count,spec in enumerate(self.scans):
+            fullName = "%s/%s_%05d.txt" % (self.folder,self.fileName,count)
+            print(fullName)
+            np.savetxt(fullName,np.column_stack((spec[0],spec[1])),fmt = "%0.7f",delimiter = "\t", header = "Wavelength (nm)\tIntensity")
+    
+    def __del__(self):
+        del self.scans
 
 class ThorlabsSpecThread(QtCore.QThread):
     acquired=QtCore.pyqtSignal(object)
@@ -79,17 +130,22 @@ class ThorlabsSpecThread(QtCore.QThread):
                 print(self.spec.get_integration_time())
         self.resume()
 
-class pyFROG_App(QtWidgets.QMainWindow):
+class thorlabsWidget(QtWidgets.QMainWindow):
     def __init__(self):
-        super(pyFROG_App,self).__init__()
+        super(thorlabsWidget,self).__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+
+        self.ThorlabsThread = None
 
         self.navi_toolbar = NavigationToolbar(self.ui.spectrumPlot, self)
         self.ui.verticalLayout.insertWidget(0,self.navi_toolbar)
         #self.threadpool = QtCore.QThreadPool()
 
-    '''def _initThorlabs(self):
+        self.ui.ConnectThorlabs.clicked.connect(self._initThorlabs)
+        self.ui.in_specexposure.textChanged.connect(lambda: self.ThorlabsThread.updateThorlabsParameters(self.ui.in_specexposure.text(),param ="integrationTime"))
+
+    def _initThorlabs(self):
         if self.ThorlabsThread:
             print("Thorlabs Spectrometer has already been initialized")
         else:
@@ -97,14 +153,22 @@ class pyFROG_App(QtWidgets.QMainWindow):
             self.ThorlabsThread = ThorlabsSpecThread(self.ui.in_specexposure.text())
             self.wave = self.ThorlabsThread.getWavelength()
             self.intensity = self.ThorlabsThread.getIntensity()
-            self.ui.alignmentPlot.axes.cla()
-            self.alignPlot, = self.ui.alignmentPlot.axes.plot(self.wave,self.intensity)
-            self.ui.alignmentPlot.axes.set_xlabel("Wavelength [nm]")
-            self.ui.alignmentPlot.axes.set_ylabel("Intensity [a.u.]")
-            self.ui.alignmentPlot.axes.set_ylim([0,1])
+            self.ui.spectrumPlot.axes.cla()
+            self.alignPlot, = self.ui.spectrumPlot.axes.plot(self.wave,self.intensity)
+            self.ui.spectrumPlot.axes.set_xlabel("Wavelength [nm]")
+            self.ui.spectrumPlot.axes.set_ylabel("Intensity [a.u.]")
+            self.ui.spectrumPlot.axes.set_ylim([0,1])
 
             self.ThorlabsThread.acquired.connect(self.updateIntensity)
-            self.ThorlabsThread.start()'''
+            self.ThorlabsThread.start()     
+
+    def updateAlignmentPlot(self):
+        self.alignPlot.set_ydata(self.intensity)
+        self.ui.spectrumPlot.fig.canvas.draw()
+
+    def updateIntensity(self,intensity):
+        self.intensity = intensity
+        self.updateAlignmentPlot()
 
     def stopBtn(self):
         if self.spec:
@@ -117,6 +181,6 @@ if __name__ == "__main__":
     #from ResultsWindow import Results
     app = QtWidgets.QApplication(sys.argv)
     #qdarktheme.setup_theme()
-    application = pyFROG_App()
+    application = thorlabsWidget()
     application.show()
     sys.exit(app.exec_())  
